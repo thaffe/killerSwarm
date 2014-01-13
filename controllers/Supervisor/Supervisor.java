b94f56c4dc79bebd72856d2847084231a4bec80b
Julius Buset Asplin <juliusbuas@gmail.com>
2014-01-13 17:27:43 +0100

    remove comments

--- controllers/Supervisor/Supervisor.java
+++ controllers/Supervisor/Supervisor.java
@@ -1,31 +1,12 @@
-// File:          Supervisor.java
-// Date:          
-// Description:   
-// Author:        
-// Modifications: 
-
-// You may need to add other webots classes such as
-//  import com.cyberbotics.webots.controller.DistanceSensor;
-//  import com.cyberbotics.webots.controller.LED;
-// or more simply:
-//  import com.cyberbotics.webots.controller.*;
-
 import com.cyberbotics.webots.controller.Field;
 import com.cyberbotics.webots.controller.Node;
 
-// Here is the main class of your controller.
-// This class defines how to initialize and how to run your controller.
-// Note that this class derives Robot and so inherits all its functions
 public class Supervisor extends com.cyberbotics.webots.controller.Supervisor {
     private static final int ROBOTS = 8;
     private static final double BOARD_DIM = 2;
-    // You may need to define your own functions or variables, like
-    //  private LED led;
 
-    // Supervisor constructor
     public Supervisor() {
 
-        // call the Robot constructor
         super();
         for (int i = 1; i <= ROBOTS; i++) {
             Node node = getFromDef("EPuck"+i);
@@ -35,31 +16,14 @@ public class Supervisor extends com.cyberbotics.webots.controller.Supervisor {
             field.setSFVec3f(new double[]{x, 0, y});
 
         }
-
-        // You should insert a getDevice-like function in order to get the
-        // instance of a device of the robot. Something like:
-        //  led = getLED("ledName");
-
     }
 
-    // This is the main program of your controller.
-    // It creates an instance of your Robot subclass, launches its
-    // function(s) and destroys it at the end of the execution.
-    // Note that only one instance of Robot should be created in
-    // a controller program.
-    // The arguments of the main function can be specified by the
-    // "controllerArgs" field of the Robot node
     public static void main(String[] args) {
         Supervisor controller = new Supervisor();
         controller.run();
     }
 
-    // User defined function for initializing and running
-    // the Supervisor class
     public void run(){
-        // Main loop:
-        // Perform simulation steps of 64 milliseconds
-        // and leave the loop when the simulation is over
         while (step(64) != -1) {
 
             // Read the sensors:
