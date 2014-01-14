902f43337ee121fe1be558c6794293834c73f3f9
Julius Buset Asplin <juliusbuas@gmail.com>
2014-01-14 14:53:21 +0100

    Create empty methods

--- controllers/EPuckController/EPuckController.java
+++ controllers/EPuckController/EPuckController.java
@@ -1,12 +1,22 @@
+import com.cyberbotics.webots.controller.*;
+import com.cyberbotics.webots.controller.Camera;
 import com.cyberbotics.webots.controller.DifferentialWheels;
 import com.cyberbotics.webots.controller.Field;
+import com.cyberbotics.webots.controller.LED;
 import com.cyberbotics.webots.controller.Node;
+
+import java.lang.String;
 
 public class EPuckController extends DifferentialWheels {
 
+    private LED led;
+    private Camera camera;
+
+    private int[] image;
+    private float[] rangeImage;
+
     public EPuckController() {
         super();
-
     }
 
     public static void main(String[] args) {
@@ -17,7 +27,48 @@ public class EPuckController extends DifferentialWheels {
     public void run() {
         this.setSpeed(100, -100);
         while (step(64) != -1) {
+            this.rangeImage = this.camera.getRangeImage();
+            this.image = this.camera.getImage();
 
+            this.avoidObjects();
         }
+    }
+
+    private void avoidObjects(){
+
+    }
+
+    private void wander(){
+
+    }
+
+    private void explore(){
+
+    }
+
+    private void buildMaps(){
+
+    }
+
+    private void monitorChanges(){
+
+    }
+
+    private void identifyObjects(){
+
+    }
+
+    private void planChangesToTheWorld(){
+
+    }
+
+    private void reasonAboutBehaviourOfObject(){
+
+    }
+
+    private void setLed(int red, int green, int blue){
+        String hex = "";
+
+        this.led.set(Integer.parseInt(hex, 16));
     }
 }
