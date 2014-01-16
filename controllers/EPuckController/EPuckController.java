2828a3b99e8407eafdc52c57aa6fc1ef7259813a
Nicolay Thafvelin <thaffe1401@gmail.com>
2014-01-16 14:15:57 +0100

    Ting

--- controllers/EPuckController/EPuckController.java
+++ controllers/EPuckController/EPuckController.java
@@ -1,37 +1,30 @@
-import com.cyberbotics.webots.controller.*;
 import com.cyberbotics.webots.controller.Camera;
 import com.cyberbotics.webots.controller.DifferentialWheels;
 import com.cyberbotics.webots.controller.DistanceSensor;
-import com.cyberbotics.webots.controller.Field;
-import com.cyberbotics.webots.controller.LED;
-import com.cyberbotics.webots.controller.Node;
-
-import java.lang.String;
 
 public class EPuckController extends DifferentialWheels {
-
+    private final static int DIST_SENSOR_FL = 6, DIST_SENSOR_FFL = 7, DIST_SENSOR_L = 5,
+            DIST_SENSOR_FR = 1, DIST_SENSOR_FFR = 0, DIST_SENSOR_R = 2;
+    private final static int DIST_THRESHOLD = 300;
+    private final static int TIME_STEP = 32;
+    private final static int MAX_SPEED = 1000;
     private Camera camera;
-    private DistanceSensor pSensorL, pSensorFL, pSensorFR, pSensorR;
-
     private double rSpeed, lSpeed;
-    private double rx1 = 250, rx2 = 250, rx3 = 250, rx4 = 250;
-    private double lx1 = 250, lx2 = 250, lx3 = 250, lx4 = 250;
-
     private int[] image;
     private float[] rangeImage;
+    private DistanceSensor[] distSensors;
+    private double[] distValues;
 
     public EPuckController() {
         super();
+        this.distSensors = new DistanceSensor[8];
+        this.distValues = new double[8];
+        for (int i = 0; i < distSensors.length; i++) {
+            distSensors[i] = this.getDistanceSensor("ps" + i);
+            distSensors[i].enable(TIME_STEP);
+        }
         this.camera = this.getCamera("camera");
-        this.pSensorL = this.getDistanceSensor("ps6");
-        this.pSensorFL = this.getDistanceSensor("ps7");
-        this.pSensorFR = this.getDistanceSensor("ps0");
-        this.pSensorR = this.getDistanceSensor("ps1");
-        this.pSensorFL.enable(16);
-        this.pSensorL.enable(16);
-        this.pSensorR.enable(16);
-        this.pSensorFR.enable(16);
-        this.camera.enable(16);
+        this.camera.enable(TIME_STEP);
     }
 
     public static void main(String[] args) {
@@ -41,66 +34,78 @@ public class EPuckController extends DifferentialWheels {
 
     public void run() {
         this.setSpeed(100, -100);
-        while (step(16) != -1) {
+        while (step(TIME_STEP) != -1) {
+            //Update distances sensors
+            for (int i = 0; i < distSensors.length; i++) {
+                distValues[i] = distSensors[i].getValue();
+            }
             this.rangeImage = this.camera.getRangeImage();
             this.image = this.camera.getImage();
 
             this.avoidObjects();
-            this.setSpeed(rSpeed,lSpeed);
+            this.setSpeed(lSpeed, rSpeed);
         }
     }
 
-    private void avoidObjects(){
-        double left = pSensorL.getValue() + pSensorFL.getValue();
-        double right = pSensorR.getValue() + pSensorFR.getValue();
-        if(left > 300 || right > 300){
-            if(left > right && rSpeed != -300){
-                rSpeed = 1000;
-                lSpeed = -300;
-            }
-            else if(lSpeed != -300){
-                rSpeed = -300;
-                lSpeed = 1000;
-            }
+    private void avoidObjects() {
+        double left = distValues[DIST_SENSOR_FL] + distValues[DIST_SENSOR_FFL];
+        double right = distValues[DIST_SENSOR_FR] + distValues[DIST_SENSOR_FFR];
 
+        if (left > DIST_THRESHOLD || right > DIST_THRESHOLD) {
+            if(left > right && lSpeed > 0)
+                turn(true);
+            else if(rSpeed > 0)
+                turn(false);
+        } else {
+            drive(true);
         }
-        else{
-            lSpeed = 1000;
-            rSpeed = 1000;
-        }
+    }
 
-        //rSpeed = rx1 / pSensorL.getValue() + rx2 / pSensorFL.getValue() + rx3 / pSensorFR.getValue() + rx4 / pSensorR.getValue();
-        //lSpeed = lx1 / pSensorL.getValue() + lx2 / pSensorFL.getValue() + lx3 / pSensorFR.getValue() + lx4 / pSensorR.getValue();
-     }
-
-    private void wander(){
+    private void wander() {
 
     }
 
-    private void explore(){
+    private void explore() {
 
     }
 
-    private void buildMaps(){
+    private void buildMaps() {
 
     }
 
-    private void monitorChanges(){
+    private void monitorChanges() {
 
     }
 
-    private void identifyObjects(){
+    private void identifyObjects() {
 
     }
 
-    private void planChangesToTheWorld(){
+    private void planChangesToTheWorld() {
 
     }
 
-    private void reasonAboutBehaviourOfObject(){
+    private void reasonAboutBehaviourOfObject() {
 
     }
 
-    private void setLed(int red, int green, int blue){
+    private void setLed(int red, int green, int blue) {
     }
+
+    private void turn(boolean right) {
+        this.lSpeed = right ? MAX_SPEED : -MAX_SPEED * 0.5;
+        this.rSpeed = !right ? MAX_SPEED : -MAX_SPEED * 0.5;
+    }
+
+    private void drive(boolean forward) {
+        this.lSpeed = forward ? MAX_SPEED : -MAX_SPEED;
+        this.rSpeed = forward ? MAX_SPEED : -MAX_SPEED;
+    }
+
+    private void stop() {
+        this.lSpeed = 0;
+        this.rSpeed = 0;
+    }
+
+
 }
