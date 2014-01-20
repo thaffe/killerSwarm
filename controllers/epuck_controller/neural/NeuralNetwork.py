be3b2299116adf722f28eed04fc8a73f87765ab6
Julius Buset Asplin <juliusbuas@gmail.com>
2014-01-20 19:14:16 +0100

    removed invisible layer

--- controllers/epuck_controller/neural/NeuralNetwork.py
+++ controllers/epuck_controller/neural/NeuralNetwork.py
@@ -1,8 +1,9 @@
 import Neuron
 
+
 class NeuralNetwork:
 
-    def __init__(self, inputCount, invisibleLayers = 0, outputCount = 1, weights = 0, min = -1, max = 1):
+    def __init__(self, inputCount, outputCount = 1, weights = 0, min = -1, max = 1):
         self.inputs = [Neuron([]) for i in range(inputCount)]
         self.outputs = []
         for i in range(outputCount):
@@ -10,7 +11,7 @@ class NeuralNetwork:
             for j in range(inputCount):
                 inputs.append(Neuron.Input(self.inputs[j], weights[i][j]))
 
-            self.outputs.append(Neuron(inputs))
+            self.outputs.append(Neuron(inputs, min, max))
 
     def update(self, inputs):
         for i in range(len(inputs)):
